127b4a9d4ad1f6725fd598ac9847bf2ca939b2a8
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-11 22:40:58 -0400

    Added Compare Triplets

---
+++ public/HackerRank/CompareTriplets.js
@@ -0,0 +1,17 @@
+function compareTriplets(a, b) {
+    let scoreArray = []
+    let aScore = 0
+    let bScore = 0
+    
+    for (let i = 0; i < 3; i++) {
+      if (a[i] > b[i]) {
+          aScore += 1
+      } else if (a[i] < b[i]) {
+          bScore += 1
+        }
+    }
+      scoreArray.push(aScore)
+      scoreArray.push(bScore)
+      return scoreArray
+  
+  }
