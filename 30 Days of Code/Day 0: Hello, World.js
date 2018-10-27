12ce0a647e11f229d93194833f1044eecc4d8586
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-10-26 23:16:28 -0400

    Added Challenge

---
+++ 30 Days of Code/Day 0: Hello, World.js
@@ -0,0 +1,5 @@
+function processData(inputString) {
+    let firstLine = "Hello, World.";
+    process.stdout.write(firstLine + '\n' + inputString)
+    let secondLine = "Welcome to 30 Days of Code";
+} 
